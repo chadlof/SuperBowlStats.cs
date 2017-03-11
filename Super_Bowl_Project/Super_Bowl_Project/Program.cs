33a1d929aef1ca1c89c4b137ee4492705dbcf88f
Brent Clark <brentmclark@gmail.com>
2017-03-10 21:36:50 -0600

    Move instructions to README

--- Super_Bowl_Project/Super_Bowl_Project/Program.cs
+++ Super_Bowl_Project/Super_Bowl_Project/Program.cs
@@ -8,59 +8,6 @@ using System.IO;
 
 namespace Super_Bowl_Project
 {
-    // Your job for this project is to create an application that will quantify and summarize data from the excel sheet titled 
-    //“Super_Bowl_Projectl.csv”.  Construct an application that will read data from the Super_Bowl_Project.csv spreadsheet and 
-    //assign each of the row item as an object within your program.  Once your program has created an instance for each row within 
-    //the spreadsheet begin to manipulate the data according to the bullet points below.  Once you have constructed code for the 
-    //following bullet points generate a text file that outputs the quantified and summarized data.
-
-    //!Generate a list of all super bowl winners and output the following:
-
-    //The team name
-    //The Year the team won
-    //The winning quarterback
-    //The winning coach
-    //The MVP
-    //The point difference between the winning pts and losing pts.
-
-    //Generate a list of the top five attended super bowl’s and output the following:
-
-    //The year
-    //The winning team
-    //The losing team
-    //The city
-    //The state
-    //The stadium
-
-    //Generate a list of states that hosted the most super bowls and output the following:
-
-    //The city
-    //The state
-    //The stadium
-
-    //Generate a list of players of won MVP more than twice and output the following:
-
-    //Name of MVP
-    //The winning team
-    //The losing team
-
-    //For the below questions, output a single value:
-
-    //Which coach lost the most super bowls?
-    //Which coach won the most super bowls?
-    //Which team won the most super bowls?
-    //Which team lost the most super bowls?
-    //Which Super bowl had the greatest point difference?
-    //What has been the average attendance of all super bowls?
-
-
-    //Program Considerations
-
-    //Please include a title for each output to the text file along with proper spacing
-
-    //Allow the end-user to select the desired file path where text file will output (the program must not fail if path does not exist)
-
-    //Your program should read the file from where the project folder exist.
     class Program
     {
         static void Main(string[] args)
